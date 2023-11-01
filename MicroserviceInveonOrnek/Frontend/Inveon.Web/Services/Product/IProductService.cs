5d1b6030c084f81fe388678eb9f06a2b09aa6d0d
snnehir <snnehir21@gmail.com>
2023-11-01 16:48:25 +0300

    project init

---
+++ MicroserviceInveonOrnek/Frontend/Inveon.Web/Services/Product/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using Inveon.Web.Models;
+using Inveon.Web.Services.IServices;
+
+namespace Inveon.Web.Services.Product
+{
+    public interface IProductService : IBaseService
+    {
+        Task<T> GetAllProductsAsync<T>(string token);
+        Task<T> GetProductByIdAsync<T>(int id, string token);
+        Task<T> CreateProductAsync<T>(ProductDto productDto, string token);
+        Task<T> UpdateProductAsync<T>(ProductDto productDto, string token);
+        Task<T> DeleteProductAsync<T>(int id, string token);
+    }
+
+}
